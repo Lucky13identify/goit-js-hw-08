77dafcf8709873c969115e7be36eb2be503fc94e
Lucky13identify <108291368+Lucky13identify@users.noreply.github.com>
2023-02-01 18:10:14 +0000

    Deploying to gh-pages from @ Lucky13identify/goit-js-hw-08@c62557a0705d1a0194de600bab90c2cf99270869 🚀

--- 02-video.695b86f0.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector("iframe"),t=new Player(e);t.on("timeupdate",throttle((function(){t.getCurrentTime().then((function(e){localStorage.setItem("videoplayer-current-time",e)})).catch((function(e){}))}),1e3)),t.setCurrentTime(localStorage.getItem("videoplayer-current-time"));
-//# sourceMappingURL=02-video.695b86f0.js.map
