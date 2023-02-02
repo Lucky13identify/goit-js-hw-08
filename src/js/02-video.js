151894cb049af573471e0349e3f7a76373a76463
Lucky13identify <108291368+Lucky13identify@users.noreply.github.com>
2023-02-02 13:24:02 +0200

    new

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -4,23 +4,30 @@ import throttle from 'lodash.throttle';
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 
+setTime();
 player.on('timeupdate', throttle(localeStorageTyping, 1000));
 
 // Функция записывает отслеживаемое событие и записывает его (секунды),
 //  как значение в локалСторедж
 
 function localeStorageTyping() {
-  player
-    .getCurrentTime()
-    .then(function (seconds) {
-      localStorage.setItem('videoplayer-current-time', seconds);
-      // seconds = the current playback position
-    })
-    .catch(function (error) {
-      // an error occurred
-    });
+  player.getCurrentTime().then(function (seconds) {
+    localStorage.setItem('videoplayer-current-time', seconds);
+    // seconds = the current playback position
+  });
 }
 
 // Вызываем метод для изменения текущего времени видео после перезагрузки страницы
 
-player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
+// player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
+
+function setTime() {
+  const currentTime = Number(localStorage.getItem('videoplayer-current-time'));
+
+  try {
+    player.setCurrentTime(currentTime);
+  } catch (error) {
+    // window.alert(error.name, error.message);
+    player.setCurrentTime(currentTime);
+  }
+}
